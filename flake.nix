6ca417d8fdb70baa97d02d8580d38e891f504674
David Haines <dhaines@gmail.com>
2023-07-07 11:27:28 -0400

    add chaos

--- flake.nix
+++ flake.nix
@@ -45,6 +45,19 @@
           }
         ];
       };
+      chaos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/chaos
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.dhaines = import ./home.nix;
+          }
+        ];
+      };
     };
   };
 }
